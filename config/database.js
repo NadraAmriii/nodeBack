16508c0ca8243598e29770f75e1b61e966c85213
nadra <nadraamri568@gmail.com>
2022-06-05 18:09:27 +0200

    bugs colse

--- config/database.js
+++ config/database.js
@@ -1,12 +1,15 @@
 //Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://NadraAmry:nadra123@cluster0.zdrqyat.mongodb.net/dossierJoueurs?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_SERVER_URL || "mongodb+srv://NadraAmry:nadra123@cluster0.zdrqyat.mongodb.net/dossierJoueurs?retryWrites=true&w=majority"
 const connectionParams={
   useNewUrlParser: true,
   useUnifiedTopology: true 
 }
 mongoose.connect(mongoDB,connectionParams,(err) => {
-  console.log(`successfully connected to database ${err}`);
+  if(err){
+    console.log(err)
+  }
+  console.log(`successfully connected to database`);
 })
 
 mongoose.Promise = global.Promise;
